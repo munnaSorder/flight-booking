20b27a80c1343048371b3fcbf99efeea52922fe7
munnaSorder <developermunnaa@gmail.com>
2021-02-19 23:07:59 +0600

    almost done

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+//ticket increase and decrease function
 function ticketCost(count, classes){
     const userClass = document.getElementById(classes+ 'Class');
     const userClassValue = userClass.value;
-
+    const userClassNewValue = parseInt(userClassValue)
     if(count == true){
-        const userClassNewValue = parseInt(userClassValue)  + 1;
-        userClass.value = userClassNewValue;
+        userClass.value = userClassNewValue + 1;
+        handelTicketCost();
     }
     if(count == false && userClass.value > 0){
-        const userClassNewValue = parseInt(userClassValue)  - 1;
-        userClass.value = userClassNewValue;
+        userClass.value = userClassNewValue - 1;
+        handelTicketCost();
     }
+}
+
+//total ticket & other cost calculation function
+function handelTicketCost(){
+    //first Class Ticket Cost Calculation
+    const firstClass = document.getElementById('firstClass');
+    const firstClassValue = firstClass.value;
+    const totalFirstClassCost = parseInt(firstClassValue) * 150;
+
+    //economy class total cost calculation
+    const economyClass = document.getElementById('economyClass');
+    const economyClassValue = economyClass.value;
+    const totalEconomyClass = parseInt(economyClassValue) * 100;
+
+    //total Ticket cost Calculation
+    const totalTicketCost = totalFirstClassCost + totalEconomyClass;
+
+    //govVat Calculation
+    const govVat =  Math.round(totalTicketCost) * 0.1;
+
+    //grand Total Calculation
+    const grandTotal = totalTicketCost + govVat;
+
+    //print result
+    document.getElementById('subTotal').innerText = '$' + totalTicketCost;
+    document.getElementById('vat').innerText = '$' + govVat;
+    document.getElementById('grandTotal').innerText = '$' + grandTotal;
 }
